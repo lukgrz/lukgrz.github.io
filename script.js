f24f2171852b71dca734fe666e0d93ee7e1b6722
lukgrz <lukaszgrzegorzak@gmail.com>
2021-09-21 11:02:36 +0200

    trello apiroot fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    const apiRoot = 'https://cryptic-scrubland-05205.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'https://cryptic-scrubland-05205.herokuapp.com/v1/task/getTasks';
+   const trelloApiRoot = 'https://cryptic-scrubland-05205.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
